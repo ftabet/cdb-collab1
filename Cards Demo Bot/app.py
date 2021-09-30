19c8cc1fe4e1513b76addbeed08b2f101dd3ea4d
ftabet <91638716+ftabet@users.noreply.github.com>
2021-09-30 18:10:20 -0500

    Add files via upload

---
+++ Cards Demo Bot/app.py
@@ -0,0 +1,68 @@
+from flask import Flask, request
+from webexteamssdk import WebexTeamsAPI, Webhook 
+from cardcontent import *
+import smartsheet
+
+app = Flask(__name__)
+api = WebexTeamsAPI(access_token="MDVjMzM3ZDYtZmIwYS00NTkwLWIyNjUtYjViZWYwNjc5NTVhODFjY2YxNDYtNjc3_PF84_0337a4e5-3f50-4247-bde9-c97809b174cc") 
+
+@app.route('/', methods=['POST', 'GET']) 
+def home():
+	return 'OK', 200 
+
+@app.route('/webhookreq', methods=['POST', 'GET']) 
+def webhookreq(): 
+    if request.method == 'POST':
+        req = request.get_json()
+		
+        data_personId = req['data']['personId']
+        data_roomId = req['data']['roomId']
+
+			
+		#Loop prevention VERY IMPORTANT!
+        me = api.people.me()
+        if data_personId == me.id:
+            return 'OK', 200
+        else:
+            if api.messages.create(roomId=data_roomId, text='Fact# 1 Freddy is the Best', attachments=[{"contentType": "application/vnd.microsoft.card.adaptive", "content": cardcontent}]):
+                return "OK"
+    elif request.method == 'GET':
+        return "Yes, this is working."
+    return 'OK', 200 
+
+@app.route('/cardsubmitted', methods=['POST'])
+def cardsubmitted():
+    if request.method == 'POST':
+        req = request.get_json()
+
+        data_id = req['data']['id']
+
+        attachment_actions = api.attachment_actions.get(data_id)
+        inputs = attachment_actions.inputs
+
+        myName = inputs['myName']
+        myEmail = inputs['myEmail']
+        myTel = inputs['myTel']
+
+        print(myName)
+        print(myEmail)
+        print(myTel)
+        smart = smartsheet.Smartsheet('ghQqTSkHzIA9OIu5eEjcgj6v5rz9Pwj2ei4zF') #Smartsheet Access Token
+        smart.errors_as_exceptions(True)
+
+        # Specify cell values for the added row                         
+        newRow = smartsheet.models.Row()
+        newRow.to_top = True
+      # The above variables are the incoming JSON
+        newRow.cells.append({ 'column_id': 8865247346878340, 'value': myName })                   #  
+        newRow.cells.append({ 'column_id': 4783860184573828, 'value': myEmail, 'strict': False })   
+        newRow.cells.append({ 'column_id': 7035659998259076, 'value': myTel, 'strict': False }) 
+        response = smart.Sheets.add_rows(366990838785924, newRow) # The -- xxxxxxxxxxxxxx -- on this line is the sheet ID.
+    return 'OK', 200 
+
+
+
+if __name__=='__main__':
+	app.debug = True 
+	app.run(host="0.0.0.0")
+
